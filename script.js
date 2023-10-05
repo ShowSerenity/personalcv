170ed9818dcf7f0f8818b5d7a62a65d430789f3b
ShowSerenity <asanali.rymgali@gmail.com>
2023-10-05 23:40:40 +0600

    upgraded to bootstrap

--- script.js
+++ script.js
@@ -11,3 +11,41 @@ projectBlocks.forEach((block) => {
         window.location.href = url;
     });
 });
+
+
+
+
+// Showing tooltip on icon hover
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+
+
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
